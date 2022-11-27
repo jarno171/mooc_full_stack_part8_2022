bd81683d800d7dd42c6277e3b891ed73b0d8e346
Jarno Varis <jarno.varis@gmail.com>
2022-11-27 16:13:16 +0200

    updating born year seems to work, and returns null if author was not found, like in exercise

--- library-backend.js
+++ library-backend.js
@@ -123,6 +123,7 @@ const typeDefs = gql`
       author: String!
       genres: [String!]
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -181,6 +182,17 @@ const resolvers = {
       const book = { ...args, id: uuid()}
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name)
+
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((author) => author.name === args.name ? updatedAuthor: author)
+      return updatedAuthor
     }
   }
 }
